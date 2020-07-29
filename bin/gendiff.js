dd63c4e4bf7830a28c488a1ef76601568c76dee4
alex.rdn <cher.alex97@gmail.com>
2020-07-29 13:24:49 +0300

    Add format option and args

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,7 @@ import program from 'commander';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference')
-  .parse(process.argv);
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
+
+program.parse(process.argv);
