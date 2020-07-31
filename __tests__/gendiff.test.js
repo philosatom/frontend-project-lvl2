b1415ae3ea73730818b1441ddd6e6000044fb0d9
alex.rdn <cher.alex97@gmail.com>
2020-07-31 12:08:40 +0300

    Add yml test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,22 +6,31 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
+const expected = [
+  '{',
+  '  - follow: false',
+  '    host: hexlet.io',
+  '  - proxy: 123.234.53.22',
+  '  - timeout: 50',
+  '  + timeout: 20',
+  '  + verbose: true',
+  '}'
+].join('\n');
 
 test('gendiff json', () => {
   const path1 = getFixturePath('before.json');
   const path2 = getFixturePath('after.json');
 
   const actual = genDiff(path1, path2);
-  const expected = [
-    '{',
-    '  - follow: false',
-    '    host: hexlet.io',
-    '  - proxy: 123.234.53.22',
-    '  - timeout: 50',
-    '  + timeout: 20',
-    '  + verbose: true',
-    '}'
-  ].join('\n');
+
+  expect(actual).toBe(expected);
+});
+
+test('gendiff yml', () => {
+  const path1 = getFixturePath('before.yml');
+  const path2 = getFixturePath('after.yml');
+
+  const actual = genDiff(path1, path2);
 
   expect(actual).toBe(expected);
 });
